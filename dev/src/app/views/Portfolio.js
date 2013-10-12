2cd3f62147b44082031a64bf58754de3eff47c95
Nicolas Pigelet <nico@nicolaspigelet.com>
2013-10-12 23:41:07 +0100

    loading of the big images

--- dev/src/app/views/Portfolio.js
+++ dev/src/app/views/Portfolio.js
@@ -6,8 +6,12 @@ AC.View.Portfolio = AC.View.Base.extend({
 	initedLinks : false,
 	slug : null,
 	detailSwipe : null,
-	preload : null,
-	preloadedAll : false,
+
+	preloadImg : null,
+	preloadedImgAll : false,
+
+	preloadBG : null,
+	preloadedBGAll : false,
 
 	initialize : function () {
 		this.params.projects = AC.Data.JSON.portfolio;
@@ -15,8 +19,11 @@ AC.View.Portfolio = AC.View.Base.extend({
 
 	hide : function(callback) {
 		
-		if ( $("body").data("all-loaded") === true ) 
-			this.preloadedAll = true;
+		if ( $("body").data("all-img-loaded") === true ) 
+			this.preloadedImgAll = true;
+		
+		if ( $("body").data("all-bg-loaded") === true ) 
+			this.preloadedBGAll = true;
 
 		if (callback) {
 			callback();
@@ -34,8 +41,11 @@ AC.View.Portfolio = AC.View.Base.extend({
 			callback : this._callbackSwipe
 		});
 
-		if ( !this.preloadedAll ) this.initPreload();
+		if ( !this.preloadedImgAll ) this.initImgPreload();
 		else this.prepImages();
+
+		if ( !this.preloadedBGAll ) this.initBGPreload();
+		else this.prepBG();
 
 		this.initProjectNav();
 	},
@@ -44,7 +54,7 @@ AC.View.Portfolio = AC.View.Base.extend({
 		$(".current-index .current").html( index + 1 );
 	},
 
-	initPreload : function() {
+	initImgPreload : function() {
 
 		var 
 			o,
@@ -59,14 +69,39 @@ AC.View.Portfolio = AC.View.Base.extend({
 			manifest.push(o);
 		});
 
-		this.preload = new createjs.LoadQueue(true);
-		this.preload.addEventListener("fileload", this.handleFileLoad );
-		this.preload.addEventListener("complete", this.handleComplete);
-		this.preload.loadManifest(manifest);
+		this.preloadImg = new createjs.LoadQueue(true);
+		this.preloadImg.addEventListener("fileload", this.handleFileLoad );
+		this.preloadImg.addEventListener("complete", this.handleComplete );
+		this.preloadImg.loadManifest(manifest);
+	},
+
+	initBGPreload : function() {
+
+		var 
+			o,
+			manifest = [];
+
+		$(".to-load-bg").each(function(index, el) {
+			
+			o = {};
+			o.src = $(el).data("img");
+			o.id = $(el).data("img");
+
+			manifest.push(o);
+		});
+
+		this.preloadBG = new createjs.LoadQueue(true);
+		this.preloadBG.addEventListener("fileload", this.handleFileLoadBG );
+		this.preloadBG.addEventListener("complete", this.handleCompleteBG );
+		this.preloadBG.loadManifest(manifest);
 	},
 
 	handleComplete : function() {
-		$("body").data("all-loaded", true);
+		$("body").data("all-img-loaded", true);
+	},
+
+	handleCompleteBG : function() {
+		$("body").data("all-bg-loaded", true);
 	},
 
 	handleFileLoad : function (event) {
@@ -77,6 +112,14 @@ AC.View.Portfolio = AC.View.Base.extend({
 			.addClass("loaded");
 	},
 
+	handleFileLoadBG : function (event) {
+
+		$("[data-img='" + event.item.id + "']")
+			.removeClass("to-load-bg")
+			.css("background-image", "url(" + event.item.src + ")")
+			.addClass("loaded-bg");
+	},
+
 	prepImages : function() {
 
 		$(".to-load").each(function(index, el) {
@@ -84,6 +127,17 @@ AC.View.Portfolio = AC.View.Base.extend({
 				.removeClass("to-load")
 				.attr("src", $(el).data('src'))
 				.addClass("loaded");
+		});
+	},
+
+	prepBG : function() {
+
+		$(".to-load-bg").each(function(index, el) {
+			
+			$(el)
+				.removeClass("to-load-bg")
+				.css("background-image", "url(" + $(el).data('img') + ")")
+				.addClass("loaded-bg");
 		});
 	},
 	
