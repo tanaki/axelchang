bbfc6273fc4393fe92e579a6e5333c5f45e70833
Nicolas Pigelet <nico@nicolaspigelet.com>
2013-10-28 19:13:59 +0000

    update path for testing

--- build/index.php
+++ build/index.php
@@ -70,9 +70,10 @@
 			AC.Locations.Root = '<?php echo $BASE_URL; ?>';
 			AC.Locations.Templates = '<?php echo $BASE_URL; ?>templates/';
 			AC.Locations.Images = '<?php echo $BASE_URL; ?>img/';
-			AC.Locations.JSON = '<?php echo $BASE_URL; ?>data/data.json';
+			//AC.Locations.JSON = '<?php echo $BASE_URL; ?>data/data.json';
 			//AC.Locations.JSON = '<?php echo ( $PROD ? "/koken/" : $BASE_URL . "data/koken_proxy.php"); ?>';
-			// AC.Locations.JSON = '<?php echo $BASE_URL; ?>data/koken_proxy.php';
+			//AC.Locations.JSON = '<?php echo $BASE_URL; ?>data/koken_proxy.php';
+			AC.Locations.JSON = '<?php echo ( $PROD ? "/koken/" : $BASE_URL . "data/data.json"); ?>';
 		</script>
 		
 	</body>
