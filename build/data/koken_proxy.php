9190707db4361dd9933da4c5bc6d0f83aeaf0680
Nicolas Pigelet <nico@nicolaspigelet.com>
2013-11-15 23:29:28 +0000

    debug koken

--- build/data/koken_proxy.php
+++ build/data/koken_proxy.php
@@ -121,8 +121,9 @@
 	
 
 	function clean($string) {
-		$string = str_replace('', '-', $string); // Replaces all spaces with hyphens.
-		return preg_replace('/[^A-Za-z0-9 \-]/', '', $string); // Removes special chars.
+		//$string = str_replace('', '-', $string); // Replaces all spaces with hyphens.
+		//return preg_replace('/[^A-Za-z0-9 \-\[\]\"]/', '', $string); // Removes special chars.
+		return htmlentities(trim($string));
 	}
 
 	function encodeAccent($temp) {
